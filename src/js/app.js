b35582c6fc8d86b3cfe89954bdc5ca333a4b6252
wilfer.yepes <wilferyepes@hotmail.com>
2023-11-07 12:25:43 -0500

    Bienes Raices

---
+++ src/js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function() {
+    eventListeners();
+    darkMode();
+});
+
+function darkMode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // console.log(prefiereDarkMode.matches);
+
+    //preferencias de usuario de windows
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    }else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function(){
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        }else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+    });
+}
+
+function eventListeners() {
+    const mobilMenu = document.querySelector('.mobile-menu');
+
+    mobilMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive(){
+    const navegacion = document.querySelector('.navegacion');
+
+    // if(navegacion.classList.contains('mostrar')) {
+    //     navegacion.classList.remove('mostrar')
+    // }else {
+    //     navegacion.classList.add('mostrar')
+    // }
+    navegacion.classList.toggle('mostrar')
+}
